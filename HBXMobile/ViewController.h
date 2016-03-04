67649833d5aa7e06c4713fa518bfccc209ca422e
David Boyd <dboyd5@davids-macbook-pro.local>
2016-03-04 15:49:28 -0500

    Initial Commit

---
+++ HBXMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBXMobile
+//
+//  Created by David Boyd on 3/4/16.
+//  Copyright © 2016 David Boyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
